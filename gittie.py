cdda4ad0e24ab80e52243a69b359f57613a1e308
piotrtar <piotr.tarczon@gmail.com>
2017-04-20 10:38:09 +0200

    Changes 2

--- gittie.py
+++ gittie.py
@@ -2,3 +2,13 @@ def printer():
     print("Hello")
 
 printer()
+
+def add():
+    print("This will add 2 numbers")
+    number1 = int(input('Enter number 1: '))
+    
+    number2 = int(input('Enter number 2: '))
+    print(number1 + number2)
+
+add()
+    
